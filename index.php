037a329c9f135172ea6ea9a917b5cf0f7a6868ae
europagymnasium <you@example.com>
2017-03-07 14:53:11 +0100

    fixed bug with levels

--- index.php
+++ index.php
@@ -78,12 +78,11 @@ var PreloadState = {
         game.load.image('smoke3', 'assets/smoke3.png');
         game.load.image('smoke4', 'assets/smoke4.png'); 
         game.load.audio('jump', 'assets/jump.ogg');
-<<<<<<< HEAD
+
         game.load.audio('theme', 'assets/mariotheme.ogg');
-        game.load.tilemap('level11', './assets/level1.json',null, Phaser.Tilemap.TILED_JSON); 
-=======
+
         game.load.tilemap('level1', './assets/level1.json',null, Phaser.Tilemap.TILED_JSON); 
->>>>>>> 20e10efebbc06172874489a33bffd6cb0f3461aa
+
         game.load.tilemap('level2', './assets/level2.json',null, Phaser.Tilemap.TILED_JSON); 
         game.load.image('tileset', './assets/tileset.png');
         game.load.image('ice-terrain', './assets/ice-terrain.png');
